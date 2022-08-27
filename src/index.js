964345e1c7e8a70e05f399d32e8f65ee2650977c
Konstantin7898 <lexus7898@gmail.com>
2022-08-27 22:29:35 +0300

    add handlebars

--- src/index.js
+++ src/index.js
@@ -41,17 +41,43 @@ function onInputSearch(e) {
             'beforeend',
             oneCountry(countriesArray)
           );
+          refs.countryListEl.innerHTML = '';
         } else {
           refs.countryInfoEl.insertAdjacentHTML(
             'beforeend',
             countryMarkup(countriesArray)
           );
-          console.log(refs.countryInfoEl);
         }
       })
       .catch(err => {
         console.log(err);
         Notiflix.Notify.failure('Oops, there is no country with that name');
+        return [];
       });
   }
+}
+function countryMarkup(countries) {
+  return countries
+    .map(element => {
+      return `<div class='one-country'>
+        <img width="60px" height="60px" src='${element.flags.svg}' alt='${
+        element.name.common
+      } flag' />
+        <p class="country-name">${element.name.official}</p></div>
+        <p class="additional-info"><b>Capital:</b> ${element.capital}</p>
+        <p class="additional-info"><b>Population:</b> ${element.population}</p>
+        <p class="additional-info"><b>Languages:</b> ${Object.values(
+          element.languages
+        )}</p>`;
+    })
+    .join('');
+}
+function oneCountry(countries) {
+  return countries
+    .map(element => {
+      return `<li class="multiple-countries">
+      <img width="60px" height="60px" src="${element.flags.svg}" alt="${element.name.common} flag" />
+      <p class="country-name">${element.name.common}</p></li>`;
+    })
+    .join('');
 }
